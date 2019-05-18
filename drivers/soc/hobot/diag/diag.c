f88c681dd97639677e6621ae36b2ad9824b177f0
chenbo <bo01.chen@horizon.ai>
2019-05-18 11:33:11 +0800

    soc:diag: update diag driver(send environment data)

--- drivers/soc/hobot/diag/diag.c
+++ drivers/soc/hobot/diag/diag.c
@@ -4,7 +4,7 @@
  ****			 All rights reserved.             ****
  *************************************************************/
 /**
- * netlink module,  freight station.
+ * netlink module,  freight station
  * @author		bo01.chen(bo01.chen@horizon.ai)
  * @date		2019/4/11
  */
@@ -13,6 +13,7 @@
 #include <linux/types.h>
 #include <net/sock.h>
 #include <linux/netlink.h>
+#include <x2/diag.h>
 
 #define MSG_LEN     200
 #define USER_PORT   100
@@ -29,11 +30,82 @@
 
 struct sock *nlsk;
 
-int send_diag_msg(char *pbuf, uint16_t len)
+static uint32_t diag_checksum(uint8_t *pbuff, uint32_t len)
+{
+	uint32_t i;
+	uint32_t totalsum;
+
+	i = len;
+	totalsum = 0;
+	while (i--)
+		totalsum += *(pbuff + i);
+
+	return totalsum;
+}
+
+/*
+ * due to the discontinuity of memory addresses
+ * of struct diag_msg, so, copy diag msg to a
+ * continuous memory area
+ * @msg diag_msg
+ */
+static uint8_t *diag_msg_copy_to_buffer(diag_msg *msg)
+{
+	uint8_t *p;
+
+	p = vmalloc(sizeof(diag_msg_hdr) + msg->head.len + 4);
+	if (p == NULL) {
+		diag_error("vmallc faile\n");
+		goto exit;
+	}
+
+	memcpy(p, (uint8_t *)msg, sizeof(diag_msg_hdr));
+	memcpy(p + sizeof(diag_msg_hdr), msg->data, msg->head.len);
+	memcpy(p + sizeof(diag_msg_hdr) + msg->head.len,
+					(uint8_t *)&(msg->checksum), 4);
+
+exit:
+	return p;
+}
+
+/*
+ * due to the discontinuity of memory addresses
+ * of struct env_data_pack, so, copy env data
+ * pack to a continuous memory area
+ * @phead env data pack header ptr
+ * @pdata env data
+ * @len env data len
+ */
+static uint8_t *diag_msg_copy_envdata_to_buffer(
+	env_data_head * phead,
+	uint8_t *pdata,
+	uint32_t len)
+{
+	uint8_t *p;
+
+	p = vmalloc(sizeof(env_data_head) + len);
+	if (p == NULL) {
+		diag_error("%s: vmallc faile\n", __func__);
+		goto exit;
+	}
+
+	memcpy(p, phead, sizeof(env_data_head));
+	memcpy(p + sizeof(env_data_head), pdata, len);
+
+exit:
+	return p;
+}
+
+/*
+ * use netlink to send data to the userspace.
+ * @pbuf data ptr
+ * @len data len
+ * @return -1:error, >=0:OK
+ */
+static int diag_send_msg(char *pbuf, uint32_t len)
 {
 	struct sk_buff *nl_skb;
 	struct nlmsghdr *nlh;
-
 	int ret = 0;
 
 	/* create netlink skbuffer. */
@@ -54,25 +126,182 @@ int send_diag_msg(char *pbuf, uint16_t len)
 	/* send data*/
 	memcpy(nlmsg_data(nlh), pbuf, len);
 	ret = netlink_unicast(nlsk, nl_skb, USER_PORT, MSG_DONTWAIT);
+	if (ret < 0) {
+		diag_error("diag_send_msg %d bytes\n", len);
+		return -1;
+	}
 
-	return ret;
+	return len;
 }
-EXPORT_SYMBOL(send_diag_msg);
 
+/*
+ * send event sta and it's env data to the diag app.
+ * @event event id
+ * @sta event sta
+ * @env_data_type When is the environmental data generated.
+ * @env_data env data
+ * @env_len env data len.
+ * @return -1:error, >=0:OK
+ */
+static DEFINE_MUTEX(diag_msg_snd_mutex);
+int diag_send_event_stat_and_env_data(
+	diag_event_id event,
+	diag_event_stat sta,
+	env_data_type env_typ,
+	uint8_t *env_data,
+	size_t env_len)
+{
+	diag_msg msg;
+	uint8_t *p;
+	uint8_t *penvbuff;
+	uint8_t ver;
+	size_t length;
+	size_t total_len = 0;
+	report_event_sta_pack event_pack;
+	env_data_pack envpack;
+	uint32_t frag_cnt;
+	uint32_t i;
+	uint32_t actual_snd_data_len;
+	uint32_t actual_snd_data_rsv_len;
+
+	mutex_lock(&diag_msg_snd_mutex);
+
+	if (event >= EVENT_MAX ||
+		sta == DIAG_EVENT_UNKNOWN) {
+		goto error;
+	}
+
+	/*
+	 * event and it's stat report.
+	 */
+	memset((uint8_t *)&msg, 0x00, sizeof(diag_msg));
+	msg.head.packt_ident = DIAG_MSG_HEAD;
+	ver = DIAG_MSG_VER;
+	msg.head.version |= (ver & 0x0f);
+	msg.head.version |= ((~ver << 4) & 0xf0);
+	msg.head.type = REPORT_EVENT_STAT;
+	event_pack.event_id = event;
+	event_pack.sta = sta;
+	msg.head.len = sizeof(report_event_sta_pack);
+	msg.data = (uint8_t *)&event_pack;
+	p = diag_msg_copy_to_buffer(&msg);
+	if (p == NULL) {
+		diag_error("diag_msg_copy_to_buffer return NULL\n");
+		goto error;
+	}
+	length = sizeof(diag_msg_hdr) + msg.head.len;
+	msg.checksum = diag_checksum(p, length);
+	memcpy(p + length, &(msg.checksum), 4);
+	diag_send_msg(p, length + 4);
+	vfree(p);
+	total_len += length + 4;
+
+	/*
+	 * if env data exist, then send env data.
+	 */
+	if (env_len == 0 || env_data == NULL) {
+		diag_error("env len = 0 or env data is nULL\n");
+		goto error;
+	}
+
+	/* set env data pack */
+	msg.head.type = SEND_ENV_DATA;
+	envpack.head.pack_info.event_id = event;
+	envpack.head.pack_info.sta = sta;
+	envpack.head.type = env_typ;
+	envpack.data = env_data;
+
+	//env_len += sizeof(env_data_head);
+	actual_snd_data_len = FRAGMENT_SIZE - sizeof(env_data_head);
+	actual_snd_data_rsv_len = env_len % actual_snd_data_len;
+	frag_cnt = env_len / actual_snd_data_len;
+	if (actual_snd_data_rsv_len != 0)
+		frag_cnt++;
+
+	diag_debug("%d fragment will send\n", frag_cnt);
+	/* send each fragment.*/
+	for (i = 0; i < frag_cnt; i++) {
+		if (i == 0)
+			msg.head.start = 0x01;
+		else
+			msg.head.start = 0x00;
+
+		if (i == (frag_cnt - 1))
+			msg.head.end = 0x01;
+		else
+			msg.head.end = 0x00;
+
+		msg.head.seq = i;
+
+		if (i == (frag_cnt - 1))
+			msg.head.len = sizeof(env_data_head) +
+						actual_snd_data_rsv_len;
+		else
+			msg.head.len = FRAGMENT_SIZE;
+
+		penvbuff = diag_msg_copy_envdata_to_buffer(
+			&(envpack.head),
+			envpack.data + i*actual_snd_data_len,
+			(i == (frag_cnt - 1))?actual_snd_data_rsv_len :
+			actual_snd_data_len);
+
+		if (penvbuff == NULL) {
+			diag_error("penvbuff at %d return NULL\n", i);
+			goto error;
+		}
+
+		msg.data = penvbuff;
+		p = diag_msg_copy_to_buffer(&msg);
+		vfree(penvbuff);
+		if (p == NULL) {
+			diag_error("copy msg to buffer\%d return NULL\n", i);
+			goto error;
+		}
+
+		length = sizeof(diag_msg_hdr) + msg.head.len;
+		msg.checksum = diag_checksum(p, length);
+		memcpy(p + length, &(msg.checksum), 4);
+		diag_send_msg(p, length + 4);
+		vfree(p);
+		total_len += length + 4;
+	}
+
+	mutex_unlock(&diag_msg_snd_mutex);
+	return total_len;
+
+error:
+	mutex_unlock(&diag_msg_snd_mutex);
+	return -1;
+}
+EXPORT_SYMBOL(diag_send_event_stat_and_env_data);
+
+/*
+ * diag rcv msg from userspace.
+ */
 static void netlink_rcv_msg(struct sk_buff *skb)
 {
 	struct nlmsghdr *nlh = NULL;
 	char *umsg = NULL;
-	char *kmsg = "kernel space: hello users!!!";
+	//char *kmsg = "kernel space: hello users!!!";
+	unsigned char *data;
+
+	data = vmalloc(0x500000);
+	memset(data, 0x01, 0x500000);
+	if (!data) {
+		diag_error("vmalloc fail\n");
+		return;
+	}
 
 	if (skb->len >= nlmsg_total_size(0)) {
 		nlh = nlmsg_hdr(skb);
 		umsg = NLMSG_DATA(nlh);
 		if (umsg) {
-			diag_debug("userspace: %s\n", umsg);
-			send_diag_msg(kmsg, strlen(kmsg));
+			diag_error("userspace: %s\n", umsg);
+			//send_diag_msg(data, 0x500000);
 		}
 	}
+
+	vfree(data);
 }
 
 struct netlink_kernel_cfg cfg = {
@@ -82,11 +311,15 @@ struct netlink_kernel_cfg cfg = {
 int diag_netlink_init(void)
 {
 	/* create netlink socket */
-	nlsk = netlink_kernel_create(&init_net, NETLINK_DIAG, &cfg);
+	nlsk = (struct sock *)netlink_kernel_create(
+		&init_net,
+		NETLINK_DIAG,
+		&cfg);
 	if (nlsk == NULL) {
-		diag_error("netlink_kernel_create error\n");
+		diag_error("netlink_kernel_create error !\n");
 		return -1;
 	}
+
 	diag_debug("diag netlink init\n");
 	return 0;
 }
@@ -103,6 +336,7 @@ void diag_netlink_exit(void)
 module_init(diag_netlink_init);
 module_exit(diag_netlink_exit);
 
-MODULE_LICENSE("GPL v2");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("bo01.chen@horizon.ai");
 MODULE_DESCRIPTION("fusa netlink module");
+
--- include/x2/diag.h
+++ include/x2/diag.h
@@ -1,31 +1,139 @@
 /***************************************************************************
- * COPYRIGHT NOTICE
- * Copyright 2019 Horizon Robotics, Inc.
- * All rights reserved.
- **************************************************************************/
-/**
- * @file     diag.h
- * @brief    diag api(send diag msg to usrspace)
- * @author   chenbo(bo01.chen@horizon.ai)
- * @date     2019/4/18
- * @version  V1.0
- * @par      Horizon Robotics
- */
-#ifndef DIAG_H
-#define DIAG_H
+ *   Copyright (C) 2019 by horizon.                                        *
+ *   bo01.chen@horizon.ai                                                  *
+ *                                                                         *
+ *   Diag netlink header file.                                             *
+ *	 version: v1.0                                                         *
+ *                                                                         *
+ ***************************************************************************/
+#ifndef NETLINK_DIAG_H
+#define NETLINK_DIAG_H
+
+#define DIAG_MSG_HEAD 0xAA		// diag msg identifier.
+#define DIAG_MSG_VER 0x01		// What version of the dig msg
+#define FRAGMENT_SIZE 0x4000	// netlink msg fragment max size.
 
 extern struct net init_net;
 
+/* frame type */
+typedef enum {
+	/*kernel module or app send event id
+	 * and it's status to diag app.
+	 */
+	REPORT_EVENT_STAT = 1,
+
+	/* kernel module or app send event
+	 * corresponding env data to diag app.
+	 */
+	SEND_ENV_DATA = 2,
+
+	/* diag app send this cmd to kernel or
+	 * other app request to get env data.
+	 */
+	READ_ENV_DATA = 3,
+
+} diag_msg_type;
+
 /*
- * send diag msg to userspace
- * @pbuf: data buffer.
- * @len:  data length(how many bytes)
- * @return: -1:error, >=0:success
+ * diag msg packt header format
  */
-#ifdef CONFIG_X2_DIAG
-extern int send_diag_msg(char *pbuf, uint16_t len);
-#else
-static inline int send_diag_msg(char *buf, uint16_t len) { return 0; }
-#endif
+typedef struct {
+	uint8_t		packt_ident;
+	uint8_t		version;	// diag msg format version.
+	diag_msg_type	type;
+	uint8_t		start;		// Is diag msg start fragment.
+	uint8_t		end;		// Is diag msg end fragment.
+	uint32_t	seq;		// diag msg fragment sequence.
+	uint32_t	len;		// payload data len.
+} __packed diag_msg_hdr;
+
+/*
+ * diag msg packt format
+ */
+typedef struct {
+	diag_msg_hdr head;
+	uint8_t *data;		// payload
+
+	/* From the beginning of the packet
+	 * to the end of the data.
+	 */
+	uint32_t checksum;
+
+} __packed diag_msg;
+
+/*
+ * summary of all kernel and userspace event id.
+ */
+typedef enum {
+	IMAGE_FRAME	= 0, // range belong to kernel modules.
+	EVENT_MAX	= 11,
+
+	/* userspace app event below */
+
+} diag_event_id;
+
+/*
+ * event status.
+ */
+typedef enum {
+	DIAG_EVENT_UNKNOWN = 0,
+	DIAG_EVENT_SUCCESS = 1,
+	DIAG_EVENT_FAIL = 2,
+
+} diag_event_stat;
+
+/*
+ * event id and it's sta package,put
+ * this package in the payload of diag msg.
+ */
+typedef struct {
+	diag_event_id event_id;
+	diag_event_stat sta;
+} __packed report_event_sta_pack;
+
+/*
+ * when is the environmental data generated.
+ */
+typedef enum {
+	GEN_ENV_DATA_WHEN_ERROR = 1,
+	GEN_ENV_DATA_WHEN_RESUME_SUCCESS = 2,
+	GEN_ENV_DATA_WHEN_SUCCESS = 3,
+} env_data_type;
+
+/*
+ * env data head
+ */
+typedef struct {
+	report_event_sta_pack pack_info;
+	env_data_type type;
+
+} __packed env_data_head;
+
+/*
+ * env data structure
+ */
+typedef struct {
+	env_data_head head;
+	uint8_t *data;
+
+} __packed env_data_pack;
+
+/*
+ * send event sta and it's env data to the diag app.
+ * @event event id
+ * @sta event sta
+ * @env_data_type When is the environmental data generated.
+ * @env_data env data
+ * @env_len env data len.
+ * @return -1:error, >=0:OK
+ */
+extern int diag_send_event_stat_and_env_data(diag_event_id event,
+			diag_event_stat sta,
+			env_data_type env_typ,
+			uint8_t *env_data,
+			size_t env_len);
+
 
 #endif
+
+
