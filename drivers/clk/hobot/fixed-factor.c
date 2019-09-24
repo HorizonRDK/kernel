2ff92659da3322fcc84041f7dc40bde2e0b3e0f2
Yunqian Wang <yunqian.wang@horizon.ai>
2019-09-24 15:31:59 +0800

    clk: bug [X2J2-1409] optimize fixed-factor clk
    Summary:
    some divider must keep the frequency division ratio constant,
    	but mybe has been changed in spl or uboot.
    
    Test Plan: emmc and sd card can work porperly
    
    Reviewers: haibo.guo, xiaofeng.ling
    
    Reviewed By: haibo.guo, xiaofeng.ling
    
    Differential Revision: https://cr.hobot.cc/D8236

--- drivers/clk/hobot/fixed-factor.c
+++ drivers/clk/hobot/fixed-factor.c
@@ -3,6 +3,9 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/clkdev.h>
+#include "common.h"
+
+#define div_mask(width) ((1 << (width)) - 1)
 
 static void __init _of_x2_fixed_factor_clk_setup(struct device_node *node)
 {
@@ -10,6 +13,10 @@ static void __init _of_x2_fixed_factor_clk_setup(struct device_node *node)
 	const char* parent_name;
 	unsigned int div, mult;
 	unsigned int flags = 0;
+	unsigned int offset;
+	unsigned int field;
+	void __iomem *reg_base;
+	void __iomem *reg;
 	unsigned int val;
 	int ret;
 
@@ -19,17 +26,40 @@ static void __init _of_x2_fixed_factor_clk_setup(struct device_node *node)
 	}
 
 	parent_name = of_clk_get_parent_name(node, 0);
+	reg_base = clk_get_register_base(node);
+
+	ret = of_property_read_u32(node, "offset", &val);
+	if (!ret)
+		reg = reg_base + val;
+	else
+		reg = NULL;
+
+	ret = of_property_read_u32(node, "bits", &val);
+	if (!ret)
+		offset = val;
+
+	ret = of_property_read_u32(node, "field", &val);
+	if (!ret)
+		field = val;
 
 	ret = of_property_read_u32(node, "clk-div", &div);
-	if(ret){
-		pr_err("%s: %s missing clk-div property!\n", __func__, node->name);
+	if (ret || div < 1) {
+		pr_err("%s: %s missing clk-div property or div is invalid!\n",
+								node->name);
 		return;
 	}
 
 	ret = of_property_read_u32(node, "clk-mult", &mult);
-	if(ret){
+	if (ret) {
 		pr_err("%s: %s missing clk-mult property!\n", __func__, node->name);
 		return;
+	}
+
+	if (reg != NULL) {
+		val = clk_readl(reg);
+		val &= ~(div_mask(field) << offset);
+		val |= (div - 1) << offset;
+		clk_writel(val, reg);
 	}
 
 	ret = of_property_read_u32(node, "clk-flags", &val);
@@ -37,13 +67,13 @@ static void __init _of_x2_fixed_factor_clk_setup(struct device_node *node)
 		flags |= val;
 
 	clk_hw = clk_hw_register_fixed_factor(NULL, node->name, parent_name, flags, mult, div);
-	if(IS_ERR(clk_hw)){
+	if (IS_ERR(clk_hw)) {
 		pr_err("%s: %s failed to register the clock!\n", __func__, node->name);
 		return;
 	}
 
 	ret = clk_hw_register_clkdev(clk_hw, node->name, NULL);
-	if(ret){
+	if (ret) {
 		pr_err("%s: failed to register lookup %s!\n", __func__, node->name);
 		clk_hw_unregister_fixed_factor(clk_hw);
 		return;
