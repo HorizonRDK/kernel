cbfe9cbc146c0bf61e6727ab6c04e7b69dd53546
huawei.dong <huawei.dong@horizon.ai>
2020-02-10 18:14:32 +0800

    x3_eth: debug est
    Summary: debug x3 est function
    
    Test Plan: haved tested x3 dvb
    
    Reviewers: yunqian.wang
    
    Subscribers: huawei.dong
    
    Differential Revision: https://cr.hobot.cc/D29347

--- drivers/net/ethernet/hobot/hobot_eth.c
+++ drivers/net/ethernet/hobot/hobot_eth.c
@@ -1121,7 +1121,7 @@ static int x2_est_configuration(struct x2_priv *priv)
 	//ktime_get_real_ts64(&now);
 
 
-	x2_est_intr_config(priv);
+//	x2_est_intr_config(priv);
 
 #if 0
 	ret = x2_est_init(priv->dev, priv, &priv->plat->est_cfg, priv->dma_cap.estsel, priv->dma_cap.estdep,priv->dma_cap.estwid,priv->plat->est_en,&now);
@@ -1307,7 +1307,7 @@ int x2_tsn_link_configure(struct net_device *ndev, enum sr_class class, u16 fram
 	int err;
 	s32 bw;
 
-//	printk("%s,and into here\n", __func__);
+	printk("%s,and into here\n", __func__);
 
 	if (!x2_tsn_capable(ndev)) {
 		printk("%s: NIC not capable\n",__func__);
@@ -3187,6 +3187,7 @@ static int x2_hw_setup(struct net_device *ndev, bool init_ptp)
 			printk("PTP init failed\n");
 	}
 
+    x2_est_configuration(priv);
 	x2_set_rings_length(priv);
 	x2_start_all_dma(priv);
 
@@ -3685,11 +3686,11 @@ static int x2_set_est(struct x2_priv *priv, void __user *data)
 	ret = x2_est_configuration(priv);
 	if (!est->enabled)
 		ret = 0;
-	else {
+//	else {
 	//	printk("%s, dma_cap.fpesel:%d, plat->fp_en:%d\n", __func__, priv->dma_cap.fpesel, priv->plat->fp_en);
 //		if (priv->dma_cap.fpesel && priv->plat->fp_en)
 	//		x2_tsn_fp_configure(priv);
-	}
+//	}
 
 
 out_free:
@@ -4427,7 +4428,7 @@ static netdev_tx_t x2_xmit(struct sk_buff *skb, struct net_device *ndev)
 		}
 	}
 
-	//printk("%s,and queue:%d\n",__func__,queue);
+//	printk("%s,and queue:%d\n",__func__,queue);
 //	printk("%s, and avali:%d, nfrags:%d, queue:%d\n",__func__, x2_tx_avail(priv, queue), nfrags,queue);
 	if ((x2_tx_avail(priv, queue) < nfrags + 1)) {
 		if (!netif_tx_queue_stopped(netdev_get_tx_queue(ndev, queue))) {
@@ -6081,7 +6082,7 @@ static int x2_eth_dwmac_config_dt(struct platform_device *pdev, struct plat_conf
 	plat_dat->dma_cfg->aal = 1;
 	plat_dat->pmt = 1;
     plat_dat->fp_en = true;
-//    plat_dat->est_en = false;
+    plat_dat->est_en = false;
 	return 0;
 }
 
