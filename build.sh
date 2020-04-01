35e02926a06f5a093d8313e1605628818a1807ea
Zhang Guoying <guoying.zhang@horizon.ai>
2020-04-01 20:35:59 +0800

    config: config build bpu new driver as module
    Summary: Signed-off-by: Zhang Guoying <guoying.zhang@horizon.ai>
    
    Test Plan: no plan
    
    Reviewers: ming.yu
    
    Reviewed By: ming.yu
    
    Differential Revision: https://cr.hobot.cc/D36311

--- build.sh
+++ build.sh
@@ -260,6 +260,8 @@ function all()
     cpfiles "$SRC_KERNEL_DIR/drivers/crypto/hobot/pka/elprsa.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
     cpfiles "$SRC_KERNEL_DIR/drivers/crypto/hobot/core/spaccexample.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
     cpfiles "$SRC_KERNEL_DIR/drivers/soc/hobot/cnn_host/hobot_cnn_host_total.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
+	cpfiles "$SRC_KERNEL_DIR/drivers/soc/hobot/bpu/bpu_framework.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
+	cpfiles "$SRC_KERNEL_DIR/drivers/soc/hobot/bpu/bpu_cores.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
     cpfiles "$SRC_KERNEL_DIR/drivers/misc/hobot_efuse.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
     cpfiles "$SRC_KERNEL_DIR/drivers/misc/x2_efuse.ko " "$TARGET_TMPROOTFS_DIR/lib/modules/"
     cpfiles "$SRC_KERNEL_DIR/_install/lib/modules/*" "$TARGET_TMPROOTFS_DIR/lib/modules/"
