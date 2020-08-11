47bfe690eb632b5321b591ec2a68e02b1cc0dc8a
jianghe.xu <jianghe.xu@horizon.ai>
2020-08-11 18:59:58 +0800

    kernel: usb: dwc3: license: rollback the license header to Texas Instruments Incorporated.
    Summary:
    rollback the license header to Texas Instruments Incorporated.
    
    Just rollback the license header.
    
    Test Plan: ok
    
    Reviewers: huiliang.xu, zenghao.zhang
    
    Subscribers: jianghe.xu
    
    Differential Revision: https://cr.hobot.cc/D52278

--- drivers/usb/dwc3/core.c
+++ drivers/usb/dwc3/core.c
@@ -1,13 +1,22 @@
-/*
- * Horizon Robotics
+/**
+ * core.c - DesignWare USB3 DRD Controller Core file
  *
- *  Copyright (C) 2020 Horizon Robotics Inc.
- *  All rights reserved.
+ * Copyright (C) 2010-2011 Texas Instruments Incorporated - http://www.ti.com
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Authors: Felipe Balbi <balbi@ti.com>,
+ *         Sebastian Andrzej Siewior <bigeasy@linutronix.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2  of
+ * the License as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <linux/clk.h>
