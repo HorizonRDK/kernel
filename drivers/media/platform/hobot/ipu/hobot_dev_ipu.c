fe1c736f83530204a7fcd2c9f9b62288621d24b8
zhenjian.he <zhenjian.he@horizon.ai>
2021-01-03 23:03:34 +0800

    media: ipu|pym: bug [XJ3-2552] fix dq repeated frames
    Summary:
    in the multi process scene：
    when a secondary process just joins, maybe get two repeated frames due to poll return POLLIN twice, so:
    1.when a process has polled, it will set the event and wake up (except ipu src)
    2.remove the POLLIN return without event judge
    
    Test Plan: pass
    
    Reviewers: xingzhu.gong, xiaofeng.ling
    
    Reviewed By: xiaofeng.ling
    
    Differential Revision: https://cr.hobot.cc/D70024

--- drivers/media/platform/hobot/ipu/hobot_dev_ipu.c
+++ drivers/media/platform/hobot/ipu/hobot_dev_ipu.c
@@ -443,8 +443,6 @@ static u32 x3_ipu_poll(struct file *file, struct poll_table_struct *wait)
 	if (!list_empty(done_list)) {
 		ipu->statistic.pollin_comp[ipu_ctx->group->instance]\
 			[ipu_ctx->id]++;
-		framemgr_x_barrier_irqr(framemgr, 0, flags);
-		return POLLIN;
 	}
 	framemgr_x_barrier_irqr(framemgr, 0, flags);
 	if(ipu_ctx->event == VIO_FRAME_DONE) {
@@ -3192,6 +3190,7 @@ void ipu_frame_done(struct ipu_subdev *subdev)
 	u32 event = 0;
 	int cache_bufindex;
 	struct vio_frame *cache_frame;
+	unsigned long poll_mask = 0;
 
 	group = subdev->group;
 	ipu = subdev->ipu_dev;
@@ -3216,6 +3215,7 @@ void ipu_frame_done(struct ipu_subdev *subdev)
 		event = VIO_FRAME_DONE;
 		trans_frame(framemgr, frame, FS_COMPLETE);
 		frame->poll_mask = subdev->poll_mask;
+		poll_mask = subdev->poll_mask;
 		subdev->poll_mask = 0x00;
 		ipu->statistic.fe_normal[group->instance][subdev->id]++;
 
@@ -3254,7 +3254,12 @@ void ipu_frame_done(struct ipu_subdev *subdev)
 
 	spin_lock_irqsave(&subdev->slock, flags);
 	for (i = 0; i < VIO_MAX_SUB_PROCESS; i++) {
-		if (test_bit(i, &subdev->val_ctx_mask)) {
+		/*
+		 * wake up process when already poll except ipu src
+		 */
+		if (test_bit(i, &subdev->val_ctx_mask) &&
+				(test_bit(i, &poll_mask) ||
+				subdev->id == GROUP_ID_SRC)) {
 			ipu_ctx = subdev->ctx[i];
 			if (ipu_ctx) {
 				ipu_ctx->event = event;
