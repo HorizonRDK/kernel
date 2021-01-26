6c22e91bb3f0308ab581c7e8a547b563c65e8192
jianghe.xu <jianghe.xu@horizon.ai>
2021-01-26 16:46:34 +0800

    usb: uvc: bug [XJ3-2738] fix the issue of cltr-c cause uvc-gadget warning
    Summary:
    Fix the issue that, cltr-c close uvc-gadget but meet
    driver warning. And the next time uvc-gadget is failed to pullup...
    
    Keep disconnect behind uvcg_video_enable,
    to avoid dwc3_gadget_run_stop timeout issue in some case.
    eg. host media app close, gadget will have 4 queued urb blocked in
    usb controller. Then usb controller couldn't stop in this situation.
    Therefore, move uvcg_video_enable(video, 0) in front, which will
    do usb_ep_dequeue for every blocked urb.
    
    In summary, the main reason that why usb controller can't stop with
    DCTL command is that still some urb blocked in IN endpoints.
    And we need to do usb_ep_dequeue before stop usb controller.
    
    Test Plan: ok
    
    Reviewers: yadong.li, zenghao.zhang, xiaofeng.ling
    
    Reviewed By: yadong.li
    
    Subscribers: jianghe.xu
    
    Differential Revision: https://cr.hobot.cc/D73181

--- drivers/usb/gadget/function/uvc_v4l2.c
+++ drivers/usb/gadget/function/uvc_v4l2.c
@@ -329,12 +329,20 @@ uvc_v4l2_release(struct file *file)
 	struct uvc_file_handle *handle = to_uvc_file_handle(file->private_data);
 	struct uvc_video *video = handle->device;
 
-	uvc_function_disconnect(uvc);
-
 	mutex_lock(&video->mutex);
 	uvcg_video_enable(video, 0);
 	uvcg_free_buffers(&video->queue);
 	mutex_unlock(&video->mutex);
+
+	/*
+	 * keep disconnect behind uvcg_video_enable,
+	 * to avoid dwc3_gadget_run_stop timeout issue in some case.
+	 * eg. host media app close, gadget will have 4 queued urb blocked in
+	 * usb controller. Then usb controller couldn't stop in this situation.
+	 * Therefore, move uvcg_video_enable(video, 0) in front, which will
+	 * do usb_ep_dequeue for every blocked urb.
+	 */
+	uvc_function_disconnect(uvc);
 
 	file->private_data = NULL;
 	v4l2_fh_del(&handle->vfh);
