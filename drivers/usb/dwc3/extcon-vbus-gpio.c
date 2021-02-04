806de25a81b63aa828f386e3bc5a47f7901a426e
kun.yan <kun.yan@horizon.ai>
2021-02-04 19:34:22 +0800

    usb: gadget: modify Copyright info
    Summary: usb: gadget: modify Copyright info
    
    Test Plan: pass
    
    Reviewers: jianghe.xu
    
    Reviewed By: jianghe.xu
    
    Subscribers: kun.yan
    
    Differential Revision: https://cr.hobot.cc/D74373

--- drivers/usb/dwc3/extcon-vbus-gpio.c
+++ drivers/usb/dwc3/extcon-vbus-gpio.c
@@ -1,17 +1,13 @@
-/**
- * drivers/extcon/extcon-usb-gpio.c - USB GPIO extcon driver
+/*
+ * Horizon Robotics
  *
- * Copyright (C) 2015 Texas Instruments Incorporated - http://www.ti.com
- * Author: Roger Quadros <rogerq@ti.com>
+ * Copyright (C) 2021 Horizon Robotics Inc.
+ * All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #include <linux/extcon.h>
