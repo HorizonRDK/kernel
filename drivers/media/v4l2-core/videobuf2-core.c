f6c0c24cb1ff4f89787d4b8c3336106b0743d57a
shengqiang.gao <shengqiang.gao@horizon.ai>
2021-05-20 11:24:09 +0800

    media: v4l2: task [XJ3-3190] mute sync buffer operation if using USERPTR memory type
    Summary:
    qbuf/bufdone will trigger sync buffer operation, these two sync action will takes about 2.3ms.
    isp is not need performing buffer syncing, mute it.
    
    Test Plan: isp offline ipu test pass
    
    Reviewers: jianghe.xu
    
    Differential Revision: https://cr.hobot.cc/D88622

--- drivers/media/v4l2-core/videobuf2-core.c
+++ drivers/media/v4l2-core/videobuf2-core.c
@@ -912,7 +912,8 @@ void vb2_buffer_done(struct vb2_buffer *vb, enum vb2_buffer_state state)
 	dprintk(4, "done processing on buffer %d, state: %d\n",
 			vb->index, state);
 
-	if (state != VB2_BUF_STATE_QUEUED &&
+	if (vb->vb2_queue->memory != VB2_MEMORY_USERPTR &&
+		state != VB2_BUF_STATE_QUEUED &&
 	    state != VB2_BUF_STATE_REQUEUEING) {
 		/* sync buffers */
 		for (plane = 0; plane < vb->num_planes; ++plane)
@@ -1272,8 +1273,10 @@ static int __buf_prepare(struct vb2_buffer *vb, const void *pb)
 	}
 
 	/* sync buffers */
-	for (plane = 0; plane < vb->num_planes; ++plane)
-		call_void_memop(vb, prepare, vb->planes[plane].mem_priv);
+	if (q->memory != VB2_MEMORY_USERPTR) {
+		for (plane = 0; plane < vb->num_planes; ++plane)
+			call_void_memop(vb, prepare, vb->planes[plane].mem_priv);
+	}
 
 	vb->state = VB2_BUF_STATE_PREPARED;
 
@@ -1697,8 +1700,9 @@ static void __vb2_queue_cancel(struct vb2_queue *q)
 	for (i = 0; i < q->num_buffers; ++i) {
 		struct vb2_buffer *vb = q->bufs[i];
 
-		if (vb->state == VB2_BUF_STATE_PREPARED ||
-		    vb->state == VB2_BUF_STATE_QUEUED) {
+		if (q->memory != VB2_MEMORY_USERPTR &&
+			(vb->state == VB2_BUF_STATE_PREPARED ||
+		    vb->state == VB2_BUF_STATE_QUEUED)) {
 			unsigned int plane;
 
 			for (plane = 0; plane < vb->num_planes; ++plane)
